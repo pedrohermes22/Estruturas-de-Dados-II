cdc70ba4939dfeae301435033c1c9bb0722a58e9
Israel <israel.faustino@uel.br>
2021-11-11 09:37:47 -0300

    starting graph

---
+++ src/graph.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct graphStruct{
+
+}GraphStruct;
+
+typedef struct vertexStruct{
+
+}VertexStruct;
+
+typedef struct edgeStruct{
+
+}EdgeStruct;
