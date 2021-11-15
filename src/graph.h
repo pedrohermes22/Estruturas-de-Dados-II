a2b80fcff9fedae687abcd5cb292c31dc49c6e96
Israel <israel.faustino@uel.br>
2021-11-15 18:23:12 -0300

    graph.h

--- src/graph.h
+++ src/graph.h
@@ -3,4 +3,16 @@
 
 typedef void* Graph;
 
+Graph createGraph();
+
+int destroyGraph(Graph graph);
+
+int insertVertexGraph(Graph graph, char* id, double x, double y);
+
+int insertEdgeGraph(Graph graph, char* origin, char* destiny, char* cepRight, char* cepLeft, double size, double speed, char* name);
+
+int deleteVertexGraph(Graph graph, char* id);
+
+int deleteEdgeGraph(Graph graph, char* origin, char* destiny);
+
 #endif
