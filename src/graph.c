8410edc9167b2311e6a3d50ea9c4a102e65ac485
Israel <israel.faustino@uel.br>
2021-11-15 15:30:27 -0300

    graph created

--- src/graph.c
+++ src/graph.c
@@ -4,12 +4,33 @@
 
 typedef struct graphStruct{
 
+    int amountVertex;
+    int amountEdge;
+    AdjList* adj;
+
 }GraphStruct;
 
+typedef struct adjList{
+
+    VertexStruct vertex;
+    EdgeStruct* edge;
+
+}AdjList;
+
 typedef struct vertexStruct{
+    char id[100];
+    double x;
+    double y;
 
 }VertexStruct;
 
 typedef struct edgeStruct{
+    char name[100];
+    char cepRight[100];
+    char cepLeft[100];
 
+    double size;
+    double speed;
+
+    struct edgeStruct *next;
 }EdgeStruct;
